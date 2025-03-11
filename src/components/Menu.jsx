a48a7763dcf0517347ae050e69a36c695a54431f
Nyssa Gandhi <nyssagandhi85@gmail.com>
2025-03-11 17:07:27 -0400

    Adding in general descriptions of each component file.

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Menu() {
+
+    return(
+        <div className="mt-2 mb-2 row">
+            <button className="menu_base">
+                Menu
+            </button>
+        </div>
+
+    )
+}
+
+export default Menu;
